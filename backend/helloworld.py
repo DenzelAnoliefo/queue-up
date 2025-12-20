fb31243233606c6a27178d067d5d7d7d78d0060a
DenzelAnoliefo <danoliefo@gmail.com>
2025-12-19 21:26:44 -0500

    Setup file structure. Played around with docker with a simple helloworld backend. Currrently setting up docker compose file

---
+++ backend/helloworld.py
@@ -0,0 +1,11 @@
+class Greeting:
+    def __init__(self,):
+        self.message = "Hello World!"
+
+    def print_message(self):
+        print(self.message)
+
+hello_obj = Greeting()
+
+hello_obj.print_message()
+    
